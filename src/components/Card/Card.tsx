640354e9a347baf488c2a3ff316a29adc06ba8ca
Jorge94rj <68867375+Jorge94rj@users.noreply.github.com>
2026-04-08 00:32:24 -0600

    [global]: Added Husky and prettier

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,32 +1,33 @@
-
 type CardProps = {
-    title?: string;
-    description?: string;
-    children?: React.ReactNode;
-    handleEdit?: () => void;
-    handleDelete?: () => void;
-}
+  title?: string;
+  description?: string;
+  children?: React.ReactNode;
+  handleEdit?: () => void;
+  handleDelete?: () => void;
+};
 
-const Card = ({ title, description,  children, handleEdit, handleDelete }: CardProps) => {
-
-    return (
-        <div className="cursor-pointer flex flex-col border border-white rounded-lg p-4 text-white">
-            { children }
-            <h3>{title}</h3>
-            <div className="h-32 p-2 m-4 overflow-y-auto">
-                {description}
-            </div>
-            <div className="w-full flex flex-row justify-end gap-4">
-                <button className="cursor-pointer px-2 rounded-md border border-secondary hover:bg-secondary" onClick={handleEdit}>
-                    Edit
-                </button>
-                <button className="cursor-pointer px-2 rounded-md border border-secondary hover:bg-secondary" onClick={handleDelete}>
-                    Delete
-                </button>
-            </div>
-        </div>
-    );
-
-}
+const Card = ({ title, description, children, handleEdit, handleDelete }: CardProps) => {
+  return (
+    <div className="cursor-pointer flex flex-col border border-white rounded-lg p-4 text-white">
+      {children}
+      <h3>{title}</h3>
+      <div className="h-32 p-2 m-4 overflow-y-auto">{description}</div>
+      <div className="w-full flex flex-row justify-end gap-4">
+        <button
+          className="cursor-pointer px-2 rounded-md border border-secondary hover:bg-secondary"
+          onClick={handleEdit}
+        >
+          Edit
+        </button>
+        <button
+          className="cursor-pointer px-2 rounded-md border border-secondary hover:bg-secondary"
+          onClick={handleDelete}
+        >
+          Delete
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Card;
